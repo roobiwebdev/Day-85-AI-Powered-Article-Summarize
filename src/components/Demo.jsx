15f3364ad514e3d435c0ca1f0213866bb8917fcf
roobiwebdev <mulaturhobot7@gmail.com>
2025-02-18 15:19:21 +0300

    updated animation for the writting effects

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -73,7 +73,7 @@ const Demo = () => {
             type="submit"
             className="submit_btn peer-focus:border-gray-100 peer-focus:text-gray-100"
           >
-            <p className="text-xl">↵</p>
+            <p>↵</p>
           </button>
         </form>
 
