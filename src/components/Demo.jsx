06f6adee2c0422f2926558eedd1966d294593f19
roobiwebdev <mulaturhobot7@gmail.com>
2025-02-18 12:30:12 +0300

    added animation to generated text for a writing effect

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import { useState, useEffect, useRef } from "react";
+import { Typewriter } from "react-simple-typewriter";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
@@ -8,104 +8,97 @@ const Demo = () => {
     url: "",
     summary: "",
   });
+
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
-
-  // RTK lazy query
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-  // Load data from localStorage on mount
+  const inputRef = useRef(null); // Create ref for input
+
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("articles")
+      localStorage.getItem("articles") || "[]"
     );
-
-    if (articlesFromLocalStorage) {
+    if (articlesFromLocalStorage.length > 0) {
       setAllArticles(articlesFromLocalStorage);
     }
+
+    // Focus input field when the component mounts
+    inputRef.current?.focus();
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const existingArticle = allArticles.find(
-      (item) => item.url === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
     const { data } = await getSummary({ articleUrl: article.url });
+
     if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
+      const newArticle = {
+        ...article,
+        summary: data.summary,
+      };
       const updatedAllArticles = [newArticle, ...allArticles];
 
-      // update state and local storage
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
 
-  // copy the url and toggle the icon for user feedback
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      handleSubmit(e);
-    }
+  const handleCopy = (text) => {
+    setCopied(text);
+    navigator.clipboard.writeText(text);
+    setTimeout(() => setCopied(""), 3000);
   };
 
   return (
-    <section className='mt-16 w-full max-w-xl'>
+    <section className="mt-16 w-full max-w-xl">
       {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
+      <div className="flex flex-col w-full gap-2">
         <form
-          className='relative flex justify-center items-center'
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
-
           <input
-            type='url'
-            placeholder='Paste the article link'
+            type="url"
+            placeholder="Enter An Article URL"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={handleKeyDown}
             required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+            className="url_input peer"
+            ref={inputRef} // Attach ref to input
           />
           <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
           >
-            <p>↵</p>
+            <p className="text-xl">↵</p>
           </button>
         </form>
 
-        {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+        {/* Browse URL History */}
+        <div
+          className="flex flex-col gap-1 max-h-[11rem] overflow-y-auto"
+          id="scrollbar"
+        >
           {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className='link_card'
+              className="link_card mr-2"
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
-                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate link_text">
                 {item.url}
               </p>
             </div>
@@ -113,28 +106,49 @@ const Demo = () => {
         </div>
       </div>
 
-      {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
+      {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
         {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+          <span className="loader"></span>
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-            Well, that wasn't supposed to happen...
+          <p className="font-inter font-bold text-white text-center">
+            Well, that was not supposed to happen..
             <br />
-            <span className='font-satoshi font-normal text-gray-700'>
+            <span className="font-satoshi font-normal text-gray-400">
               {error?.data?.error}
             </span>
           </p>
         ) : (
           article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
+            <div className="flex flex-col gap-3 relative">
+              <h2 className="font-satoshi font-bold text-gray-300 text-xl">
+                Article <span className="blue_gradient">Summary</span>
               </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
-                  {article.summary}
+              <div className="summary_box relative p-4 bg-gray-800 rounded-lg shadow-md">
+                {/* Typing Effect with paragraphs separated by newlines */}
+                <p className="font-inter font-medium text-sm text-gray-200 leading-relaxed whitespace-pre-wrap mb-[1em]">
+                  <Typewriter
+                    words={[article.summary]}
+                    loop={1}
+                    // cursor
+                    // cursorStyle="|"
+                    typeSpeed={10}
+                    deleteSpeed={10}
+                    delaySpeed={500}
+                  />
                 </p>
+
+                {/* Copy Button for Summary */}
+                <div
+                  className="copy_btn absolute top-2 right-2"
+                  onClick={() => handleCopy(article.summary)}
+                >
+                  <img
+                    src={copied === article.summary ? tick : copy}
+                    alt="copy_icon"
+                    className="w-5 h-5 object-contain"
+                  />
+                </div>
               </div>
             </div>
           )
